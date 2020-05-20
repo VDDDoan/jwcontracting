7e882cc346dd48f91c1c3a31f92778e05c08805b
David Doan <vandoandinh@gmail.com>
2020-05-19 22:47:14 -0700

    services page complete

--- Javascript/services/scripts.js
+++ Javascript/services/scripts.js
@@ -1,22 +1,25 @@
 //For the hamburger menu, adds responsive to the class 
+//For the hamburger menu, adds responsive to the class 
 const navSlide = () => {
-    const burger = document.querySelector('#hamburger-menu');
-    const nav = document.querySelector('.nav');
-    const navLinks = document.querySelectorAll('.nav li');
-    
-    burger.addEventListener('click', () => {
-        //toggles nav
-        nav.classList.toggle('nav-active');
-        //Animates links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation ='';
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        })
-    });
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav');
+  const navLinks = document.querySelectorAll('.nav li');
+  
+  burger.addEventListener('click', () => {
+      //toggles nav
+      nav.classList.toggle('nav-active');
+      //Animates links
+      navLinks.forEach((link, index) => {
+          if(link.style.animation){
+              link.style.animation ='';
+          }
+          else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+      });
+      //burger animation
+      burger.classList.toggle('toggle');
+  });
 }
 
 navSlide();
@@ -174,4 +177,151 @@ function showSlides2(n){
   }
   slides2[slideIndex2-1].style.display = "block";
   dots2[slideIndex2-1].className += " active";
+}
+
+// slideshow carousel script for 4th container
+var slideIndex3 = 1;
+var myTimer3;
+var slideshowContainer3;
+
+window.addEventListener("load",function() {
+    showSlides3(slideIndex3);
+    myTimer3 = setInterval(function(){plusSlides3(1)}, 4000);
+  
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides3(n){
+  clearInterval(myTimer3);
+  if (n < 0){
+    showSlides3(slideIndex3 -= 1);
+  } else {
+   showSlides3(slideIndex3 += 1); 
+  }
+  if (n === -1){
+    myTimer3 = setInterval(function(){plusSlides3(n + 2)}, 4000);
+  } else {
+    myTimer3 = setInterval(function(){plusSlides3(n + 1)}, 4000);
+  }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide3(n){
+  clearInterval(myTimer3);
+  myTimer3 = setInterval(function(){plusSlides3(n + 1)}, 4000);
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n){
+  var i;
+  var slides3 = document.getElementsByClassName("slide3");
+  var dots3 = document.getElementsByClassName("dot3");
+  if (n > slides3.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = slides3.length}
+  for (i = 0; i < slides3.length; i++) {
+      slides3[i].style.display = "none";
+  }
+  for (i = 0; i < dots3.length; i++) {
+      dots3[i].className = dots3[i].className.replace(" active", "");
+  }
+  slides3[slideIndex3-1].style.display = "block";
+  dots3[slideIndex3-1].className += " active";
+}
+
+// slideshow carousel script for 5th container
+var slideIndex4 = 1;
+var myTimer4;
+var slideshowContainer4;
+
+window.addEventListener("load",function() {
+    showSlides4(slideIndex4);
+    myTimer4 = setInterval(function(){plusSlides4(1)}, 4000);
+  
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides4(n){
+  clearInterval(myTimer4);
+  if (n < 0){
+    showSlides4(slideIndex4 -= 1);
+  } else {
+   showSlides4(slideIndex4 += 1); 
+  }
+  if (n === -1){
+    myTimer4 = setInterval(function(){plusSlides4(n + 2)}, 4000);
+  } else {
+    myTimer4 = setInterval(function(){plusSlides4(n + 1)}, 4000);
+  }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide4(n){
+  clearInterval(myTimer4);
+  myTimer4 = setInterval(function(){plusSlides4(n + 1)}, 4000);
+  showSlides4(slideIndex4 = n);
+}
+
+function showSlides4(n){
+  var i;
+  var slides4 = document.getElementsByClassName("slide4");
+  var dots4 = document.getElementsByClassName("dot4");
+  if (n > slides4.length) {slideIndex4 = 1}
+  if (n < 1) {slideIndex4 = slides4.length}
+  for (i = 0; i < slides4.length; i++) {
+      slides4[i].style.display = "none";
+  }
+  for (i = 0; i < dots4.length; i++) {
+      dots4[i].className = dots4[i].className.replace(" active", "");
+  }
+  slides4[slideIndex4-1].style.display = "block";
+  dots4[slideIndex4-1].className += " active";
+}
+
+// slideshow carousel script for 6th container
+var slideIndex5 = 1;
+var myTimer5;
+var slideshowContainer5;
+
+window.addEventListener("load",function() {
+    showSlides5(slideIndex5);
+    myTimer5 = setInterval(function(){plusSlides5(1)}, 4000);
+  
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides5(n){
+  clearInterval(myTimer5);
+  if (n < 0){
+    showSlides5(slideIndex5 -= 1);
+  } else {
+   showSlides5(slideIndex5 += 1); 
+  }
+  if (n === -1){
+    myTimer5 = setInterval(function(){plusSlides5(n + 2)}, 4000);
+  } else {
+    myTimer5 = setInterval(function(){plusSlides5(n + 1)}, 4000);
+  }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide5(n){
+  clearInterval(myTimer5);
+  myTimer5 = setInterval(function(){plusSlides5(n + 1)}, 4000);
+  showSlides5(slideIndex5 = n);
+}
+
+function showSlides5(n){
+  var i;
+  var slides5 = document.getElementsByClassName("slide5");
+  var dots5 = document.getElementsByClassName("dot5");
+  if (n > slides5.length) {slideIndex5 = 1}
+  if (n < 1) {slideIndex5 = slides5.length}
+  for (i = 0; i < slides5.length; i++) {
+      slides5[i].style.display = "none";
+  }
+  for (i = 0; i < dots5.length; i++) {
+      dots5[i].className = dots5[i].className.replace(" active", "");
+  }
+  slides5[slideIndex5-1].style.display = "block";
+  dots5[slideIndex5-1].className += " active";
 }
